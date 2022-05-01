e28eb8993070789454fec84b4944812270716527
wthoutjc <juancamilorr2012@hotmail.com>
2022-04-30 22:31:25 -0500

    fix bug

--- database/db.ts
+++ database/db.ts
@@ -39,6 +39,8 @@ const disconnect = async () => {
   if (mongoConnection.isConnected === 0) return;
 
   await mongoose.disconnect();
+  mongoConnection.isConnected = 0
+  
   console.log("Disconnected from Mongodb");
 };
 
